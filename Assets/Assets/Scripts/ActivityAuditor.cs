3357635a1402d65af5888067282f5d1cf9b8f411
Gemenim <121296811+Gemenim@users.noreply.github.com>
2023-11-22 20:33:54 +0300

    WorkOnBugs

---
+++ Assets/Assets/Scripts/ActivityAuditor.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivityAuditor : MonoBehaviour
+{
+    [SerializeField] private Transform _targetsArray;
+
+    private EnemyGenerator[] _enemyGenerator;
+    private Target[] _targets;
+    private float _cooldown = 2;
+
+    private void Awake()
+    {
+        _enemyGenerator = GetComponentsInChildren<EnemyGenerator>();
+        _targets = _targetsArray.GetComponentsInChildren<Target>();
+        SetTargets();
+    }
+
+    private void Start()
+    {        
+        var chooseSpawnerJob = ChooseSpawner();
+        StartCoroutine(chooseSpawnerJob);
+    }
+
+    private void SetTargets()
+    {
+        for (int i = 0; i < _enemyGenerator.Length; i++)
+            _enemyGenerator[i].SetTarget(_targets[i]);
+    }
+
+    private IEnumerator ChooseSpawner()
+    {
+        while (true)
+        {
+            if (CheckActivity())
+            {
+                int randomIndex = UnityEngine.Random.Range(0, _enemyGenerator.Length);
+                _enemyGenerator[randomIndex].TurnOn();
+            }
+
+            var cooldown = new WaitForSeconds(_cooldown);
+            yield return cooldown;
+        }
+    }
+
+    private bool CheckActivity()
+    {
+        bool isVerified = true;
+
+        foreach (EnemyGenerator enemyGenerator in _enemyGenerator)
+        {
+            if (enemyGenerator.IsActiv)
+            {
+                isVerified = false;
+                break;
+            }
+        }
+
+        return isVerified;
+    }
+}
---
+++ Assets/Assets/Scripts/EnemyGenerator.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    [SerializeField] private MoverEnemy _enemy;
+    [SerializeField] private float _cooldown;
+
+    private Target _target;
+    private bool _isActiv = false;
+    private float _speedEnemy = 2.0f;
+
+    public bool IsActiv => _isActiv;
+
+    private void Start()
+    {      
+        var creatEnemyJob = CreatEnemy();
+        StartCoroutine(creatEnemyJob);
+    }
+
+    private IEnumerator CreatEnemy()
+    {
+        while (true)
+        {
+            if (_isActiv)
+            {
+                var newEnemy = Instantiate(_enemy, transform.position, Quaternion.identity);
+                newEnemy.SetDirection(_target.transform, _speedEnemy);
+                _isActiv = false;
+            }
+
+            var coldown = new WaitForSeconds(_cooldown);
+            yield return coldown;
+        }
+    }
+
+    public void TurnOn()
+    {
+        _isActiv = true;
+    }
+
+    public void SetTarget(Target target)
+    {
+        _target = target;
+    }
+}
